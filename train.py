bb9695d4cc550943b97915cbe153926531b21667
catdance124 <kinosi.cat@gmail.com>
2019-05-14 09:17:08 +0900

    auto load best weight already train

--- train.py
+++ train.py
@@ -4,10 +4,12 @@ from models.pyramidNet_bottleNeck_NoRelu import resnet
 import keras
 from DataAugmentation import MyImageDataGenerator, tta
 from datetime import datetime
-nowtime = datetime.now().strftime("%y%m%d_%H%M")
+dir_name = datetime.now().strftime("%y%m%d_%H%M")
+# dir_name='190507_1700'
 import glob, os
 import pandas as pd
 import numpy as np
+import re
 
 
 if __name__ == '__main__':
@@ -26,38 +28,51 @@ if __name__ == '__main__':
 
   # data generator
   datagen = MyImageDataGenerator(
-      rotation_range=20,
-      width_shift_range=0.1,
-      height_shift_range=0.1,
-      #mix_up_alpha=1.2,
-      #random_crop=(28, 28)
-      random_erasing=True
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    #mix_up_alpha=1.2,
+    #random_crop=(28, 28)
+    random_erasing=True
   )
 
   # train
-  os.makedirs(f'./{nowtime}', exist_ok=True)
-  cp_cb = keras.callbacks.ModelCheckpoint(
-      filepath = f'./{nowtime}'+'/weights.{epoch:04d}-{loss:.6f}-{acc:.6f}-{val_loss:.6f}-{val_acc:.6f}.hdf5' , 
-      monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
-
-  batch_size=128
-  epochs=5000
-  verbose=1
+  batch_size = 128
+  initial_epoch = 0
+  epochs = 5000
+  verbose = 1
   steps_per_epoch = train_imgs.shape[0] // batch_size
 
-  history = model.fit_generator(
-      datagen.flow(train_imgs, train_lbls, batch_size=batch_size),
-      steps_per_epoch=steps_per_epoch,
-      epochs=epochs,
-      validation_data=(validation_imgs, validation_lbls),
-      callbacks=[cp_cb],
-      verbose=verbose)
+  if os.path.exists(f'./{dir_name}'):
+    best_weight_path = sorted(glob.glob(f'./{dir_name}/*.hdf5'))[-1]
+    model.load_weights(best_weight_path)
+    initial_epoch = re.search(r'weights.[0-9]{4}', best_weight_path)
+    initial_epoch = int(initial_epoch.group().replace('weights.', ''))
+  else:
+    os.makedirs(f'./{dir_name}', exist_ok=True)
+
+  if epochs > initial_epoch:
+    cp_cb = keras.callbacks.ModelCheckpoint(
+        filepath = f'./{dir_name}'+'/weights.{epoch:04d}-{loss:.6f}-{acc:.6f}-{val_loss:.6f}-{val_acc:.6f}.hdf5' , 
+        monitor='val_loss', verbose=1, save_best_only=True, mode='auto'
+    )
+
+
+    history = model.fit_generator(
+        datagen.flow(train_imgs, train_lbls, batch_size=batch_size),
+        steps_per_epoch=steps_per_epoch,
+        initial_epoch=initial_epoch,
+        epochs=epochs,
+        validation_data=(validation_imgs, validation_lbls),
+        callbacks=[cp_cb],
+        verbose=verbose
+    )
   
   # test
-  best_weight_path = sorted(glob.glob(f'./{nowtime}/*.hdf5'))[-1]
+  best_weight_path = sorted(glob.glob(f'./{dir_name}/*.hdf5'))[-1]
   model.load_weights(best_weight_path)
   
-  predicts = tta(model, test_imgs)
+  predicts = tta(model, test_imgs, tta_steps=30)
   predict_labels = np.argmax(predicts, axis=1)
 
   # create submit file
@@ -65,4 +80,4 @@ if __name__ == '__main__':
   submit.ImageId = list(range(1, predict_labels.shape[0]+1))
   submit.Label = predict_labels
 
-  submit.to_csv(f"./{nowtime}/submit{nowtime}.csv", index=False)
+  submit.to_csv(f"./{dir_name}/submit{dir_name}.csv", index=False)
